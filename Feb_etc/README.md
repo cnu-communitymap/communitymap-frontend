2103593bc5215bfee176eb2663644712b63d9d37
Yumi Heo <hymsimple@gmail.com>
2023-04-11 16:11:23 +0900

    230411 바닐라JS 기본 기능 구현

--- Feb_etc/README.md
+++
@@ -1,2 +0,0 @@
-# communitymap-frontend
-communitymap-frontend
---
+++ readpage .js
@@ -0,0 +1,7 @@
+// 글 읽기 페이지 렌더링
+// const menu_Test_Button = document.querySelector("#menuicon");
+// menu_Test_Button.addEventListener("click", function(event){
+//   event.preventDefault();
+//   console.log("sucess")
+//   renderPosts(); // 초기 게시글 렌더링합니다.
+// });
