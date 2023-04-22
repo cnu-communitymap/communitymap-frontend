3f10a7f0bc5ec7fb4a2a9624d199636bedd0cf0e
Yumi Heo <hymsimple@gmail.com>
2023-04-22 22:24:39 +0900

    init project (react, vite)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css'
+import Kakao from './components/Kakao';
+import Sidebar from "./components/Sidebar";
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Kakao />
+    </div>
+  )
+}
+
+export default App;
