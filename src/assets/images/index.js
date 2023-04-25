adaaf65b82b8b8b63847c693c3fd09b38aa84aa6
Yumi Heo <hymsimple@gmail.com>
2023-04-25 19:18:01 +0900

    category marker added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,3 +3,15 @@ import profileIcon from './profile_button.svg';
 import settingIcon from './setting_button.svg';
 
 export { menuIcon, profileIcon, settingIcon };
+
+import infoIcon from './categoryInfo.svg';
+import foodIcon from './categoryFood.svg';
+import talkIcon from './categoryTalk.svg';
+
+export { infoIcon, foodIcon, talkIcon };
+
+import infoMarkerImg from './infoMarker.svg';
+import foodMarkerImg from './foodMarker.svg';
+import talkMarkerImg from './talkMarker.svg';
+
+export { infoMarkerImg, foodMarkerImg, talkMarkerImg };
